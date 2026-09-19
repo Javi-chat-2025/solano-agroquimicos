3a30b0900f6afb7e5ec1e8f6d5f41afd146fda82
Javi-chat-2025 <al23020053@itsa.edu.mx>
2026-09-19 16:37:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1047,7 +1047,7 @@ with tab_registro:
                                     }).execute()
                                     st.success(f"Huerta '{nuevo_nombre_h}' registrada con éxito.")
                                     st.rerun()
-                                except Exception as e:
+                                mexc:
                                     st.error(f"Error al guardar huerta: {e}")
                             else:
                                 st.error("Campos obligatorios (*).")
